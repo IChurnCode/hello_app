1a682b9794889252e0465e5eab7b143cc22cf2a0
IChurnCode <rajagopal.iswarya@gmail.com>
2017-11-16 16:52:19 +0530

    second change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world! Haffun"
+    render html: "hello, world! Haffun always"
   end
 end
